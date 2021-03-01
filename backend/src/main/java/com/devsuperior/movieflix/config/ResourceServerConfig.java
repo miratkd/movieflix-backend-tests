0f1ea4327d1fb01ec5fcf055509bd08ccb71ddf9
lucas <lucas-mira1@hotmail.com>
2021-03-01 18:26:24 -0300

    GenreDTO created

--- backend/src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/movieflix/config/ResourceServerConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -28,7 +29,9 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
-	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"}; 
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**"};
+	
+	private static final String[] MEMBER_POST = {"/reviews/**"};
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -45,6 +48,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
+		.antMatchers(HttpMethod.POST, MEMBER_POST).hasRole("MEMBER")
 		.anyRequest().authenticated();
 		
 		http.cors().configurationSource(corsConfigurationSource());
